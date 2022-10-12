1cbd750534ca2a661cd800bf4e83193dfef0ba89
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-10-12 10:49:54 -0400

    add global to index

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   define:{
-    "global":{}
-  },
+ // define:{ "global":{} },
   resolve: {
     alias: {
       path: 'node_modules/path-browserify/index.js'
